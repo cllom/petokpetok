034e482fbd6c1d2958e437ceee92eb9676d1687e
mtc-20 <mamenthomas.c@gmail.com>
2022-04-08 17:08:45 +0200

    rename app to application

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
+from email.mime import application
 from website import create_app
 
-app = create_app()
+application = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
